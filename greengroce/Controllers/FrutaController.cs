be5b519d5b5f0241af993924ce7039e1a0bc4e7b
Brandom <brandom.ledesma@hotmail.com>
2022-03-31 22:45:38 -0600

    4r commit

--- greengroce/Controllers/FrutaController.cs
+++ greengroce/Controllers/FrutaController.cs
@@ -1,27 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using greengroce.Logic;
+using greengroce.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 
 namespace greengroce.Controllers
 {
+    
     public class FrutaController : Controller
     {
+        ValidateFruta objValida = new ValidateFruta();
         public IActionResult Index()
         {
             return View();
         }
-        [HttpPost]
-        public IActionResult Post([FromBody] string ClaveFruta)
+        [HttpGet]
+        public IActionResult PagFive()
         {
-            try
-            {
 
-                //objValida = new ValidateFruta(Fruta);
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            return View(objValida.Get(new Fruta()));
+       
         }
     }
 }
